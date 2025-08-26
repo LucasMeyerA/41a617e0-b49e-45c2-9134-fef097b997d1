fced80e79c5a6cc1eb9d1668a5080a1b56a79f29
LucasMeyerA <124357671+LucasMeyerA@users.noreply.github.com>
2025-08-26 16:10:50 +0200

    Create index.php
    Added PHP snippet for testing

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	echo it's written in PHP;
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SterOx customer Login</title>
+		<link rel="stylesheet" type="text/css" href="login.css">
+		<link rel="icon" type="image/x-icon" href="../icon.png">
+	</head>
+	<body>
+		
+		
+		<p> </p>
+
+	</body>
+</html>
